f825952f82210e084fc8b83c7a7f9a6543078cf0
Kaga Akatsuki <filberttkarry2210@gmail.com>
2021-07-13 11:40:59 +0700

    Update shopee.py

--- shopee.py
+++ shopee.py
@@ -39,7 +39,7 @@ class Sign(Base):
         message_list = []
         data = {}
         message = {
-            'today': today,
+            'today': '',
             'end': ''
         }
         try:
